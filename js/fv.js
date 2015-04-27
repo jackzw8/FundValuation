9456d7f4c0abaebf5c56188771784319c1add648
jackzw8 <jackzw@163.com>
2015-04-27 18:09:21 +0800

    ieda 1

--- js/fv.js
+++ js/fv.js
@@ -1,22 +1,31 @@
 /**
  * 获得基金各种数据
  */
-var FundData = {
+var FundData = (function() {  // 使用闭包，变量隐藏
 	/** 获得同花顺基金估值地址 */
-	thsValuationUrl: "http://gz.fund.10jqka.com.cn/?module=api&controller=index&action=real&codes=",
+	var thsValuationUrl = "http://gz.fund.10jqka.com.cn/?module=api&controller=index&action=real&codes=";
 
 	/** 获得好买网基金估值地址 */
-	hmValuationUrl: "http://www.howbuy.com/fund/ajax/gmfund/fundnetestimatejson.htm?jjdmstr=",
+	var hmValuationUrl = "http://www.howbuy.com/fund/ajax/gmfund/fundnetestimatejson.htm?jjdmstr=";
 
 	/** 获得天天基金估值地址,每次一个 */
-	ttValuationUrl: "http://fund.eastmoney.com/data/funddataforgznew.aspx?t=basewap&fc=",
+	var ttValuationUrl = "http://fund.eastmoney.com/data/funddataforgznew.aspx?t=basewap&fc=";
 
 	/** 获得基金的信息 */
-	fundInfoUrl: "http://fund.10jqka.com.cn/data/client/myfund/",
+	var fundInfoUrl = "http://fund.10jqka.com.cn/data/client/myfund/";
+
+	var fd = function () {
+		//构造函数
+	};
 	
 	/** 获得基金的估值 */
-	getFundValuation: function (fundlist) {
-		mui.getJ
-	}
+	fd.getFundValuation = function (fundlist) {
+		mui.getJSON(thsValuationUrl+fundlist, '', function (data) {
+			fd.fvals = JSON.parse(data);
+		})
+	};
 
-}
+	return fd;
+})();
+
+
