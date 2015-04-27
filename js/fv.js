16aacc5a7385e814c71160f7587dff1f6cf365e7
jackzw8 <jackzw@163.com>
2015-04-27 16:31:44 +0800

    1

---
+++ js/fv.js
@@ -0,0 +1,22 @@
+/**
+ * 获得基金各种数据
+ */
+var FundData = {
+	/** 获得同花顺基金估值地址 */
+	thsValuationUrl: "http://gz.fund.10jqka.com.cn/?module=api&controller=index&action=real&codes=",
+
+	/** 获得好买网基金估值地址 */
+	hmValuationUrl: "http://www.howbuy.com/fund/ajax/gmfund/fundnetestimatejson.htm?jjdmstr=",
+
+	/** 获得天天基金估值地址,每次一个 */
+	ttValuationUrl: "http://fund.eastmoney.com/data/funddataforgznew.aspx?t=basewap&fc=",
+
+	/** 获得基金的信息 */
+	fundInfoUrl: "http://fund.10jqka.com.cn/data/client/myfund/",
+	
+	/** 获得基金的估值 */
+	getFundValuation: function (fundlist) {
+		mui.getJ
+	}
+
+}
